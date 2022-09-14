ea67f9e284865dd83b8b69d47898b49d0088395a
Deep-Ramani <deepramani8505@gmail.com>
2022-09-14 12:35:59 +0530

    Create Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import MainNavigation from './components/layout/MainNavigation';
 import AllMeetupsPage from './pages/AllMeetups';
 import FavoritesPage from './pages/Favorites';
 import NewMeetupPage from './pages/NewMeetup';
@@ -7,6 +8,7 @@ import NewMeetupPage from './pages/NewMeetup';
 function App() {
   return (
     <div>
+    <MainNavigation/>
     <Routes>
      <Route path='/' element={<AllMeetupsPage/>}/>
      <Route path='/new-meetup' element={<NewMeetupPage/>}/>
